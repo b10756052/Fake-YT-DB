9fa519b4d501aef3251eb563ba5c86af951fed92
B10756052 <B10756052@gmail.com>
2022-07-30 19:39:10 +0800

    API_URL Changed

--- client/src/services/collection.services.js
+++ client/src/services/collection.services.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/collection";
+const API_URL = "https://fake-yt.herokuapp.com/api/collection";
 
 class CollectionService {
   get() {
