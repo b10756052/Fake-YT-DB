f6d95afa92e6cb12b33ab33e46ebea384aec3838
B10756052 <B10756052@gmail.com>
2022-07-30 19:29:47 +0800

    server side push try

--- server/index.js
+++ server/index.js
@@ -1,32 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
-const authRoute = require('./routes').auth;
-const collectionRoute = require('./routes').collection;
-const passport = require('passport');
-require('./config/passport')(passport);
-const cors = require("cors")
+const authRoute = require("./routes").auth;
+const collectionRoute = require("./routes").collection;
+const passport = require("passport");
+require("./config/passport")(passport);
+const cors = require("cors");
+const port = process.env.PORT || 8080;
 
 // connect to DB
-mongoose.connect(process.env.DB_CONNECT, {
+mongoose
+  .connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connect to MongoDB Altas');
-}).catch(e => {
+  })
+  .then(() => {
+    console.log("Connect to MongoDB Altas");
+  })
+  .catch((e) => {
     console.log(e);
-});
+  });
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/api/user', authRoute);
-app.use('/api/collection', passport.authenticate('jwt', { session: false }), collectionRoute)
+app.use("/api/user", authRoute);
+app.use(
+  "/api/collection",
+  passport.authenticate("jwt", { session: false }),
+  collectionRoute
+);
 
+app.get("/", (req, res) => {
+  res.send("後端架成功！");
+});
 
-app.listen(8080, (req, res) => {
-    console.log("Server is running on port 8080.");
-})
+app.listen(port, (req, res) => {
+  console.log(`Server is running on port ${port}.`);
+});
